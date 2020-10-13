ebf8261d1bcd510b2ccea9482ee24a8eb05a8d5d
khareshivang1617 <khareshivang1617@gmail.com>
2020-10-14 03:06:16 +0530

    make it better

--- djrestapi/djrestapi/settings.py
+++ djrestapi/djrestapi/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-        'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
