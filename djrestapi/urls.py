93208514ad01fc6e12d165c7ecc724aa48c105fb
khareshivang1617 <khareshivang1617@gmail.com>
2020-10-14 05:08:38 +0530

    Final

--- djrestapi/urls.py
+++ djrestapi/urls.py
@@ -17,10 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='REST API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
