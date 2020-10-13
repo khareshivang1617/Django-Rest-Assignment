c1749dc4926b3dd318b995f0cfa6f649fb61f225
khareshivang1617 <khareshivang1617@gmail.com>
2020-10-14 03:12:23 +0530

    make it better

--- djrestapi/djrestapi/settings.py
+++ djrestapi/djrestapi/settings.py
@@ -63,9 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djrestapi.urls'
@@ -156,18 +153,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 SITE_ID = 1
 
 REST_AUTH_SERIALIZERS = {'LOGIN_SERIALIZER': 'user_api.api.serializers.LoginSerializer'}
 REST_AUTH_REGISTER_SERIALIZERS = {'REGISTER_SERIALIZER': 'user_api.api.serializers.RegisterSerializer'}
 
+
+#ACCOUNT_AUTHENTICATION_METHOD = 'email'
+#ACCOUNT_EMAIL_REQUIRED = True
+#ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
